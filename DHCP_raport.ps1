f2d4f7d0e62edc662179ff1e7cd03f9a990d91aa
HarryPopov <156500288+HarryPopov@users.noreply.github.com>
2025-06-03 15:54:30 +0300

    Update DHCP_raport.ps1

--- DHCP_raport.ps1
+++ DHCP_raport.ps1
@@ -1,32 +1,31 @@
-# Impordib DHCP serveri haldamise mooduli
+# Impordi DHCP moodul
 Import-Module DHCPServer
 
-# Määrab kasutatava DHCP serveri nime (siin kasutatakse lokaalset masinat)
+# Määra serveri nimi
 $dhcpServer = "localhost"
 
-# Loob kausta raportite salvestamiseks, kui seda veel ei eksisteeri
-$folderPath = "C:\DHCP_Raport"
+# Loo kaust
+$folderPath = "C:\RAPORTID\DHCP_RAPORTID"
 if (!(Test-Path -Path $folderPath)) {
     New-Item -ItemType Directory -Path $folderPath | Out-Null
 }
 
-# Kogub DHCP teenuse oleku ja salvestab CSV faili
+# Teenuse olek
 $serviceStatus = Get-Service -Name DHCPServer | Select-Object Status, Name
 $serviceStatus | Export-Csv -Path "$folderPath\DHCP_ServiceStatus.csv" -NoTypeInformation -Encoding UTF8
 
-# Kogub DHCP v4 skoopide info ja salvestab CSV faili
+# Skoobid
 $scopes = Get-DhcpServerv4Scope -ComputerName $dhcpServer
 $scopes | Select-Object ScopeId, Name, State, StartRange, EndRange, SubnetMask |
     Export-Csv -Path "$folderPath\DHCP_Scopes.csv" -NoTypeInformation -Encoding UTF8
 
-# Funktsioon IP-aadresside vahemiku genereerimiseks (algus- ja lõppaadressi põhjal)
+# IP-aadresside vahemiku genereerimise funktsioon
 function Get-IPRange {
     param (
         [string]$startIP,
         [string]$endIP
     )
 
-    # Teisendab IP-aadressid täisarvudeks, et neid järjest läbi käia
     $start = [System.Net.IPAddress]::Parse($startIP).GetAddressBytes()
     $end = [System.Net.IPAddress]::Parse($endIP).GetAddressBytes()
     [Array]::Reverse($start)
@@ -34,7 +33,6 @@ function Get-IPRange {
     $startInt = [BitConverter]::ToUInt32($start, 0)
     $endInt = [BitConverter]::ToUInt32($end, 0)
 
-    # Loob IP-aadresside nimekirja
     $ipList = @()
     for ($i = $startInt; $i -le $endInt; $i++) {
         $bytes = [BitConverter]::GetBytes($i)
@@ -45,26 +43,20 @@ function Get-IPRange {
     return $ipList
 }
 
-# Algatab nimekirja vabade IP-aadresside kogumiseks
+# IP-de kogumine
 $freeIPs = @()
 
-# Läbib iga DHCP skoopi
 foreach ($scope in $scopes) {
-    # Kogub kõik aktiivsed rendid (leases) ja salvestab need faili
+    # Aktiivsed ühendused
     $leases = Get-DhcpServerv4Lease -ScopeId $scope.ScopeId -ComputerName $dhcpServer
     $leases | Select-Object IPAddress, ClientId, HostName, AddressState |
         Export-Csv -Path "$folderPath\Leases_$($scope.ScopeId).csv" -NoTypeInformation -Encoding UTF8
 
-    # Koostab kasutuses olevate IP-aadresside nimekirja
     $used = $leases.IPAddress
-
-    # Genereerib kogu IP-aadresside vahemiku skoopi ulatuses
     $range = Get-IPRange -startIP $scope.StartRange -endIP $scope.EndRange
 
-    # Leiab kõik vabad aadressid (ehk need, mida ei ole kasutuses)
     $available = $range | Where-Object { $used -notcontains $_ }
 
-    # Lisab iga vaba IP-aadressi objekti kujul nimekirja
     foreach ($ip in $available) {
         $freeIPs += [PSCustomObject]@{
             ScopeId = $scope.ScopeId
@@ -73,8 +65,8 @@ foreach ($scope in $scopes) {
     }
 }
 
-# Salvestab kõik vabad IP-aadressid CSV faili
+# Salvesta vabad IP-aadressid
 $freeIPs | Export-Csv -Path "$folderPath\FreeIPAddresses.csv" -NoTypeInformation -Encoding UTF8
 
-# Kuvab lõpus teate, et raport on loodud
+# Lõpuks info
 Write-Host "Raport loodud: $folderPath"
